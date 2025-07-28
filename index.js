b02314dcf70fbc5b649241fad5c8261b7f7f2305
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-28 20:17:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-
+const buttonsEl=document.querySelectorAll("button");
+for(let i=0;i<buttonsEl.length;i++)
+  {
+    buttonsEl[i].addEventListener("click",()=>{
+      const buttonValue=buttonsEl[i].textContent;
+      if(buttonValue==="C")
+clearResult()
+      else if(buttonValue==="=")
+        calculateResult()
+      else
+        appendValue(buttonValue)
+        })
+  }
+function clearResult()
+{
+ inputFieldEl.value=""; 
+}
+function calculateResult(){
+inputFieldEl.value=eval(inputFieldEl.value);
+}
+funtion appendValue(buttonValue)
+{
+  inputFieldEl.value+=buttonValue;
+}
