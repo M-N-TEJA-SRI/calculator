e399d656eb1d9781339cdf3333686447d7c864a9
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-28 20:19:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const buttonsEl=document.querySelectorAll("button");
-for(let i=0;i<buttonsEl.length;i++)
-  {
-    buttonsEl[i].addEventListener("click",()=>{
-      const buttonValue=buttonsEl[i].textContent;
-      if(buttonValue==="C")
-clearResult()
-      else if(buttonValue==="=")
-        calculateResult()
-      else
-        appendValue(buttonValue)
-        })
+const buttonsEl = document.querySelectorAll("button");
+const inputFieldEl = document.getElementById("result");
+
+buttonsEl.forEach(button => {
+  button.addEventListener("click", () => {
+    const buttonValue = button.textContent;
+
+    if (buttonValue === "C") {
+      clearResult();
+    } else if (buttonValue === "=") {
+      calculateResult();
+    } else {
+      appendValue(buttonValue);
+    }
+  });
+});
+
+function clearResult() {
+  inputFieldEl.value = "";
+}
+
+function calculateResult() {
+  try {
+    inputFieldEl.value = eval(inputFieldEl.value);
+  } catch (error) {
+    inputFieldEl.value = "Error";
   }
-function clearResult()
-{
- inputFieldEl.value=""; 
 }
-function calculateResult(){
-inputFieldEl.value=eval(inputFieldEl.value);
-}
-funtion appendValue(buttonValue)
-{
-  inputFieldEl.value+=buttonValue;
+
+function appendValue(buttonValue) {
+  inputFieldEl.value += buttonValue;
 }
